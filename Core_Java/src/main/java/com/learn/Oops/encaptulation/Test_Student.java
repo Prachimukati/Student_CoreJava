819fe484060cf9e5b51fd9bc08b11b582db90442
Prachi Mukati <prachimukati50@gmail.com>
2026-07-21 08:53:58 +0530

    ok

---
+++ Core_Java/src/main/java/com/learn/Oops/encaptulation/Test_Student.java
@@ -0,0 +1,15 @@
+package com.learn.Oops.encaptulation;
+
+public class Test_Student {
+	public static void main(String[] args) {
+		Student s = new Student();
+		s.setName("prachi");
+		s.setAge(22);
+		
+		System.out.println(s.getName());
+		System.out.println(s.getAge());
+		System.out.println("Hello");
+
+	}
+
+}
